206796dcc25a373ef5a4bdb9abb591e2adfd3bf8
Gadjev <gadjevalexandar@gmail.com>
2017-12-07 00:13:20 +0100

    Add lumen-generator

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -78,9 +78,10 @@ $app->singleton(
 |
 */
 
-// $app->register(App\Providers\AppServiceProvider::class);
-// $app->register(App\Providers\AuthServiceProvider::class);
-// $app->register(App\Providers\EventServiceProvider::class);
+     $app->register(App\Providers\AppServiceProvider::class);
+     $app->register(App\Providers\AuthServiceProvider::class);
+     $app->register(App\Providers\EventServiceProvider::class);
+     $app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------

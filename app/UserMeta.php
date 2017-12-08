48938e2c440e9c4e44c67310148674403b132b85
Gadjev <gadjevalexandar@gmail.com>
2017-12-08 13:53:42 +0100

    Add loadMeta Eloquent scope for returning user meta values

--- app/UserMeta.php
+++ app/UserMeta.php
@@ -6,6 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserMeta extends Model
 {
+
+    /**
+     * Custom scope that returns User meta_value by key
+     *
+     * @param Model $user 
+     * @param string $flag
+     * @param string $key
+     *
+     * @return string meta_value
+     */
+
+    public function scopeLoadMeta($query, $user, $key)
+    {
+        if (is_null($user) && empty($key)) {
+            return \App\User::find($user->id)->userMeta;
+        }
+
+        return $query->where(['user_id' => $user->id, 'meta_key' => $key])->get();
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);

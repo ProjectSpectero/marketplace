145e5a13c1b919a9ef23b35d6819e7ce91a34b89
Gadjev <gadjevalexandar@gmail.com>
2017-12-08 12:49:51 +0100

    Setup User-UserMeta relationship

--- app/User.php
+++ app/User.php
@@ -30,4 +30,9 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
     protected $hidden = [
         'password',
     ];
+
+    public function userMeta()
+    {
+        $this->hasMany(UserMeta::class);
+    }
 }

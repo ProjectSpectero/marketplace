dbb3c9d61d2f14b5b0a3aad02e68cda9c21c2870
Gadzev <gadjevalexandar@gmail.com>
2017-12-11 20:01:20 +0100

    Add user meta keys constants

--- app/Constants/UserMetaKeys.php
+++ app/Constants/UserMetaKeys.php
@@ -3,6 +3,15 @@
 namespace App\Constants;
 
 class UserMetaKeys {
-    
+    const Street = 'street';
+    const City = 'city';
+    const PostCode = 'post_code';
+    const PhoneNumber = 'phone_no';
+
+    static function getConstants()
+    {
+        $class = new \ReflectionClass(UserMetaKeys::class);
+        return $class->getConstants();
+    }    
 }
 

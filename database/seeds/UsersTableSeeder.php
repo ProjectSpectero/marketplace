0631bc4420bb91a5c83ec46201edda482816590a
Gadzev <gadjevalexandar@gmail.com>
2017-12-19 23:32:17 +0100

    Add static spectero user for testing

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,12 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\User::class, 5)->create();
+      factory(App\User::class, 5)->create();
+
+      \App\User::create([
+        "name" => "Spectero Dev",
+        "email" => "spectero@dev.com",
+        "password" => "$2y$10slEBapxniAtsDj85a5f1i.QMecJI.2eJbzREuLoxes1yQ5Q7RL1Da" 
+      ]);
     }
 }

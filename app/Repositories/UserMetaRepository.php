7df3ee5938186473876013cf7e8152ac8d32c482
Gadzev <gadjevalexandar@gmail.com>
2017-12-20 16:00:02 +0100

    Change addMeta method to only change the meta_value of a key if its already present

--- app/Repositories/UserMetaRepository.php
+++ app/Repositories/UserMetaRepository.php
@@ -9,6 +9,12 @@ class UserMetaRepository
 {
     static function addMeta($user, $key, $value = null)
     {
+        if (!empty(UserMeta::loadMeta($user, $key)->all())) {
+          $userMeta = UserMeta::loadMeta($user, $key)->first();
+          $userMeta->meta_value = $value;
+          $userMeta->save();
+        }        
+        
         UserMeta::create([
             'user_id' => $user->id,
             'meta_key' => $key,

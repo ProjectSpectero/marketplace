0befa3be618871ccdf18d7f8ee657d68e1f57ca4
Gadzev <gadjevalexandar@gmail.com>
2017-12-29 15:20:36 +0100

    MAR-13: Add models and migrations for node and node meta

---
+++ database/migrations/2017_12_29_142924_create_nodes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nodes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('ip');
+            $table->integer('port');
+            $table->string('access_token');
+            $table->integer('install_id');
+            $table->integer('user_id');
+            $table->string('market_model');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nodes');
+    }
+}

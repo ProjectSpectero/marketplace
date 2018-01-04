0969945418994727c3d8aec5fdff9bb86a26d2de
Paul S <contact+github@winterei.se>
2018-01-04 12:03:10 +0900

    Fix addOrUpdateMeta in UserMeta to correctly handle input.

--- app/Http/Controllers/Base/Controller.php
+++ app/Http/Controllers/Base/Controller.php
@@ -9,13 +9,7 @@ class Controller extends BaseController
 {
     public function cherryPick(Request $request, Array $validationRules)
     {
-        $curatedData = [];
-        foreach ($validationRules as $field => $rules)
-        {
-            if ($request->has($field))
-                $curatedData[$field] = $request->get($field);
-        }
-        return $curatedData;
+        return $request->only(array_keys($validationRules));
     }
 
 }

ed80e53f8e904ddfb466b62c476b26420afefc11
Paul S <contact+github@winterei.se>
2018-01-09 11:37:10 +0900

    services: fix bugs on the proxy manager, introduce a timeout.

--- app/Libraries/HTTPProxyManager.php
+++ app/Libraries/HTTPProxyManager.php
@@ -17,28 +17,31 @@ class HTTPProxyManager
         ]);
     }
 
-    public function verfiy (String $outgoingIP, String $proxyHost, String $proxyPort, String $proxyUsername, String $proxyPassword)
+    public function verify (String $outgoingIP, String $proxyHost, String $proxyPort, String $proxyUsername, String $proxyPassword)
     {
         try
         {
             $proxy = $this->formatProxy($proxyHost, $proxyPort, $proxyUsername, $proxyPassword);
-            $response = $this->client->get('/', [ 'proxy' => $proxy ]);
+            $response = $this->client->get('/', [
+                'proxy' => $proxy,
+                'connect_timeout' => env('EXTERNAL_IP_RESOLUTION_TIMEOUT_SECONDS', 5)
+            ]);
+
             if ($response->getStatusCode() == ResponseType::OK)
-            {
                 return $response->getBody()->getContents() === $outgoingIP;
-            }
         }
         catch (RequestException $exception)
         {
             // IP resolver gave something other than 200, who knows why?
+            // That or the proxy didn't work, again not our headache.
             return false;
         }
 
         return false;
     }
 
-    private function formatProxy (String $proxyHost, String $proxyPort, String $proxyUsername, String $proxyPassword) : String
+    private function formatProxy (String $proxyHost, String $proxyPort, String $proxyUsername = '', String $proxyPassword = '', String $scheme = 'http') : String
     {
-        return sprintf('http://%s:%s@%s:%d', $proxyHost, $proxyPort);
+        return sprintf('%s://%s:%s@%s:%d', $scheme, $proxyUsername, $proxyPassword, $proxyHost, $proxyPort);
     }
 }

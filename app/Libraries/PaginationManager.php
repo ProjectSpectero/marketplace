204d9e019a7213c33f0471bf59403cc6e99e7317
Paul S <contact+github@winterei.se>
2018-01-12 07:07:27 +0900

    pagination: more robust implementation.

--- app/Libraries/PaginationManager.php
+++ app/Libraries/PaginationManager.php
@@ -2,7 +2,9 @@
 
 namespace App\Libraries;
 
+use App\Constants\Errors;
 use App\Constants\ResponseType;
+use App\Errors\UserFriendlyException;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -12,13 +14,19 @@ class PaginationManager
 {
     public static function paginate(Request $request, Builder $builder) : JsonResponse
     {
+        $requestedPage = $request->get('page', 1);
         $perPage = $request->get('perPage', config('pagination.default_per_page'));
         $maxPerPage = config('pagination.max_per_page');
 
         $perPage = $perPage <= $maxPerPage ? $perPage : $maxPerPage;
 
         $paginated = $builder->paginate($perPage);
+        $paginated->appends($request->query());
         $paginatedResource = $paginated->toArray();
+
+        if ($requestedPage > $paginatedResource['last_page'])
+            throw new UserFriendlyException(Errors::REQUESTED_PAGE_DOES_NOT_EXIST);
+
         $data = $paginatedResource['data'];
         unset($paginatedResource['data']);
 

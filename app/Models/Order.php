50e07519356b00db55490e3683c85297ffb4795b
Gadzev <gadjevalexandar@gmail.com>
2018-01-16 00:27:50 +0100

    Fix Order model relations

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,6 +8,11 @@ class Order extends Model
 {
     public function lineItems()
     {
-        return $this->hasOne(OrderLineItems::class);
+        return $this->hasMany(OrderLineItems::class);
+    }
+
+    public function invoice()
+    {
+        return $this->hasOne(Invoice::class);
     }
 }

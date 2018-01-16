378e94929e5277f4af1e017835b45860c8120bd0
Paul S <contact+github@winterei.se>
2018-01-16 17:14:52 +0900

    MAR-5: provide elementary constructs for implementing transaction processing.

--- database/migrations/2018_01_16_081312_create_transactions_table.php
+++ database/migrations/2018_01_16_081312_create_transactions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateOrdersTable extends Migration
+class CreateTransactionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,8 @@ class CreateOrdersTable extends Migration
      */
     public function up()
     {
-        Schema::create('orders', function (Blueprint $table) {
+        Schema::create('transactions', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id');
-            $table->string('status');
-            $table->string('subscription_reference');
-            $table->string('subscription_provider');
             $table->timestamps();
         });
     }
@@ -30,6 +26,6 @@ class CreateOrdersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('orders');
+        Schema::dropIfExists('transactions');
     }
 }

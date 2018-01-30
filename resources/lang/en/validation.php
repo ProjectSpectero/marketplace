7a754f1d568233c8b4f3462d18539d2ca470398d
Gadzev <gadjevalexandar@gmail.com>
2018-01-30 23:13:50 +0100

    Make validation messages return keys only

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -3,11 +3,11 @@
 return [
     'required' => \App\Constants\Errors::FIELD_REQUIRED,
     'min' => [
-        'numeric' => \App\Constants\Errors::FIELD_MINLENGHT . '::attribute' .'::min',
-        'string' => \App\Constants\Errors::FIELD_MINLENGHT . '::attribute' . '::min',
+        'numeric' => \App\Constants\Errors::FIELD_MINLENGHT,
+        'string' => \App\Constants\Errors::FIELD_MINLENGHT,
     ],
     'max' => [
-        'numeric' => \App\Constants\Errors::FIELD_MAXLENGHT . '::attribute' .'::min',
-        'string' => \App\Constants\Errors::FIELD_MAXLENGHT . '::attribute' . '::min',
+        'numeric' => \App\Constants\Errors::FIELD_MAXLENGHT,
+        'string' => \App\Constants\Errors::FIELD_MAXLENGHT,
     ]
 ];

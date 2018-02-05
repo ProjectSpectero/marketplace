c46326c5b6056c8fc2d0c8cb996cca617fb233cf
Gadzev <gadjevalexandar@gmail.com>
2018-02-06 00:08:06 +0100

    Re-add mailable

---
+++ app/Mail/WelcomeWithEmailValidation.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App\Mail;
+
+
+class WelcomeWithEmailValidation extends BaseMail
+{
+
+    private $user;
+    private $verifyToken;
+
+    public function __construct($user, $verifyToken)
+    {
+        $this->user = $user;
+        $this->verifyToken = $verifyToken;
+    }
+
+    public function build()
+    {
+
+        $url = Utility::generateUrl('verify/' . $this->user->email . '/' . $this->verifyToken, 'frontend');
+
+        return $this->subject('Welcome')
+            ->markdown('emails.WelcomeWithEmailValidation', [
+                'verifyUrl' => $url,
+            ]);
+    }
+}

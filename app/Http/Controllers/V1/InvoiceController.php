c7fc933188605264455792e3918782c450ff0749
Gadzev <gadjevalexandar@gmail.com>
2018-02-06 01:29:37 +0100

    Add invoice CRUD actions

--- app/Http/Controllers/V1/InvoiceController.php
+++ app/Http/Controllers/V1/InvoiceController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\V1;
 
 use App\Constants\Errors;
+use App\Constants\Messages;
 use App\Constants\ResponseType;
 use App\Constants\UserMetaKeys;
 use App\Errors\UserFriendlyException;
@@ -10,8 +11,10 @@ use App\Invoice;
 use App\UserMeta;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
+use Illuminate\Validation\Rules\In;
 use \PDF;
 
 class InvoiceController extends CRUDController
@@ -20,6 +23,67 @@ class InvoiceController extends CRUDController
     {
         $this->resource = 'invoice';
     }
+
+    public function store(Request $request): JsonResponse
+    {
+        $rules = [
+            'order_id' => 'required',
+            'user_id' => 'required',
+            'amount' => 'required',
+            'tax' => 'required',
+            'currency' => 'sometimes',
+            'status' => 'required',
+            'due_date' => 'required'
+        ];
+
+        $this->validate($request, $rules);
+
+        $invoice = new Invoice();
+        $invoice->order_id = $request->get('order_id');
+        $invoice->user_id = $request->get('user_id');
+        $invoice->amount = $request->get('amount');
+        $invoice->tax = $request->get('tax');
+        $invoice->status = $request->get('status');
+        $invoice->due_date = $request->get('due_date');
+
+        $currency = $request->get('currency');
+        $invoice->currency = isset($currency) ? $currency : $invoice->currency;
+
+        $invoice->saveOrFail();
+
+        return $this->respond($invoice->toArray(), [], Messages::INVOICE_CREATED);
+    }
+
+    public function update(Request $request, int $id): JsonResponse
+    {
+        $invoice = Invoice::findOrFail($id);
+
+        foreach ($request as $key => $value)
+            $invoice->$key = $value;
+
+        $invoice->saveOrFail();
+
+        $this->validate($request, $id);
+
+        return $this->respond($invoice->toArray(), [], Messages::INVOICE_UPDATED);
+    }
+
+    public function destroy(Request $request, int $id): JsonResponse
+    {
+        $invoice = Invoice::findOrFail($id);
+
+        $invoice->delete();
+
+        $this->respond(null, [], Messages::INVOICE_DELETED, ResponseType::NO_CONTENT);
+    }
+
+    public function show(Request $request, int $id): JsonResponse
+    {
+        $invoice = Invoice::findOrFail($id);
+
+        return $this->respond($invoice->toArray());
+    }
+
 
     public function render (Request $request, int $id)
     {

fcf4b50edb526f35a917a0fd3f3ba2b6d0824b0c
Gadzev <gadjevalexandar@gmail.com>
2018-02-13 22:50:09 +0100

    Simplify paid invoice email template

--- resources/views/PaidInvoice.blade.php
+++ resources/views/PaidInvoice.blade.php
@@ -2,27 +2,14 @@
 
 @section ('content')
 
-    <style>
-        .container, .content {
-            max-width: 100%;
-            width: 100%;
-        }
+    <p>Thank you for your purchase</p>
 
-        .testz {
-            display: none;
-        }
-    </style>
+    <p>This is an automatically generated message to confirm receipt of your order</p>
+    <p>You do not need to reply to this e-mail, but you may wish to save it for your records.</p>
 
-    <div class="text-wrapper">
-        <p>Thank you for your purchase</p>
+    <p>Bellow you will find a copy of the invoice of your recent order</p>
 
-        <p>This is an automatically generated message to confirm receipt of your order</p>
-        <p>You do not need to reply to this e-mail, but you may wish to save it for your records.</p>
-
-        <p>Bellow you will find a copy of the invoice of your recent order</p>
-    </div>
-
-    {!! $invoice !!}
+    <a target="_blank" href="{{ $invoiceUrl }}">View Invoice</a>
 
 @endsection
 

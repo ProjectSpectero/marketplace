6570c82402943d9f0afb06d3d4ad136c6e1d8999
Gadzev <gadjevalexandar@gmail.com>
2018-02-15 01:07:26 +0100

    Add authorization for payment controller

--- app/Http/Controllers/V1/V1Controller.php
+++ app/Http/Controllers/V1/V1Controller.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\V1;
 
 use App\Constants\ResponseType;
+use App\Constants\UserRoles;
 use App\Http\Controllers\Controller;
 use App\Libraries\Utility;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\JsonResponse;
 
 class V1Controller extends Controller
@@ -51,6 +53,29 @@ class V1Controller extends Controller
                              Array $headers = [], Array $paginationParams = []) : JsonResponse
     {
         return Utility::generateResponse($data, $errors, $message, $this->version, $statusCode, $headers, $paginationParams);
+    }
+
+    public function authorizeResource (Model $model = null, String $ability = null)
+    {
+        $abilityName = $ability != null ? $ability : $this->resource . '.' . $this->getCallingMethodName();
+
+        if (\Auth::user()->isAn(UserRoles::ADMIN))
+            return true;
+
+        if ($model != null)
+            return $this->authorize($abilityName, $model);
+
+        return $this->authorize($abilityName);
+    }
+
+    private function getCallingMethodName (int $depth = 3) : String
+    {
+        $value = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, $depth);
+        $name = isset($value[2]['function']) ? $value[2]['function'] : null;
+        if ($name == null || ! in_array($name, CRUDActions::getConstants()))
+            throw new FatalException("Could not autodetermine the calling function/method name, authorization is not possible.");
+
+        return $name;
     }
 
 }

f2ff5da4135895f4d4e2c335938476162b90aa31
Gadzev <gadjevalexandar@gmail.com>
2018-02-19 23:39:34 +0100

    Add NodeGroup seeder

---
+++ database/factories/NodeGroupFactory.php
@@ -0,0 +1,11 @@
+<?php
+
+$factory->define(App\NodeGroup::class, function (Faker\Generator $faker) {
+    return [
+        'friendly_name' => $faker->colorName,
+        'status' => 'ENABLED',
+        'user_id' => $faker->numberBetween(6, 8),
+        'market_model' => array_random(\App\Constants\NodeMarketModel::getConstants()),
+        'price' => $faker->numberBetween(5, 100),
+    ];
+});
--- database/seeds/NodesTableSeeder.php
+++ database/seeds/NodesTableSeeder.php
@@ -11,6 +11,23 @@ class NodesTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Node::class, 5)->create();
+        factory(App\Node::class, 15)->create();
+        factory(App\NodeGroup::class, 5)->create();
+
+        foreach (range(1, 5) as $index)
+        {
+            \App\Node::create([
+                'ip' => '242.133.252.12'. $index,
+                'port' => 8080,
+                'protocol' => 'HTTP',
+                'access_token' => 'cloudUser' . ':' . \App\Libraries\Utility::getRandomString(),
+                'install_id' => \App\Libraries\Utility::getRandomString(),
+                'status' => array_random(\App\Constants\NodeStatus::getConstants()),
+                'user_id' => 6,
+                'price' => 9.99,
+                'market_model' => array_random(\App\Constants\NodeMarketModel::getConstants()),
+                'group_id' => null
+            ]);
+        }
     }
 }

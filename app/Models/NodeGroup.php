90850314af0eda0223dd7b1dd3e8e9774ef6399f
Gadzev <gadjevalexandar@gmail.com>
2018-02-19 23:25:50 +0100

    Eager load nodes in node groups

--- app/Models/NodeGroup.php
+++ app/Models/NodeGroup.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class NodeGroup extends BaseModel
 {
+
+    protected $with = ['nodes'];
+
     public function nodes ()
     {
         return $this->hasMany(Node::class, 'group_id');

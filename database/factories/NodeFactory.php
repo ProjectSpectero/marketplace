e4b878c2256078af5d5a2dbe8e2e1292e2cc0d01
Gadzev <gadjevalexandar@gmail.com>
2018-02-21 22:20:44 +0100

    Add invoice/node_groups/invoices generation for all active users

--- database/factories/NodeFactory.php
+++ database/factories/NodeFactory.php
@@ -8,7 +8,7 @@ $factory->define(App\Node::class, function (Faker\Generator $faker) {
         'access_token' => 'cloudUser' . ':' . \App\Libraries\Utility::getRandomString(),
         'install_id' => $faker->sha256,
         'status' => array_random(\App\Constants\NodeStatus::getConstants()),
-        'user_id' => $faker->numberBetween(6, 8),
+        'user_id' => $faker->numberBetween(6, 10),
         'price' => $faker->numberBetween(5, 100),
         'market_model' => array_random(\App\Constants\NodeMarketModel::getConstants()),
         'group_id' => $faker->numberBetween(1, 5)

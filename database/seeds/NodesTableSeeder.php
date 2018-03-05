24b75dfe310ac2e2ab9568356065bf4369039800
Paul S <contact+github@winterei.se>
2018-03-05 16:22:47 +0900

    Fix seeders to make nodes under a group be owned by the same user as the group.

--- database/seeds/NodesTableSeeder.php
+++ database/seeds/NodesTableSeeder.php
@@ -11,13 +11,25 @@ class NodesTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Node::class, 25)->create();
-        factory(App\NodeGroup::class, 5)->create();
+        factory(App\Node::class, 100)->create();
+        factory(App\NodeGroup::class, 25)->create();
 
         foreach (\App\Node::all()->random(10) as $node)
         {
             $node->group_id = null;
             $node->save();
+        }
+
+        foreach (\App\NodeGroup::all() as $group)
+        {
+            foreach ($group->nodes() as $node)
+            {
+                if ($group->user_id != $node->user_id)
+                {
+                    $node->user_id = $group->user_id;
+                    $node->saveOrFail();
+                }
+            }
         }
     }
 }

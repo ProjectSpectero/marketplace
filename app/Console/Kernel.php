104caccd8600a245fd7ef6ca0453cc2df70c564f
Gadzev <gadjevalexandar@gmail.com>
2018-03-14 22:41:31 +0100

    MAR-40: Add weekly schedule for GeoIP DB Updates

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -60,5 +60,10 @@ class Kernel extends ConsoleKernel
             }
 
         })->daily();
+
+        $schedule->exec('geoipupdate -d ' . base_path() . '/resources/geoip')
+            ->weekly()
+            ->sundays()
+            ->timezone('America/Los_Angeles');
     }
 }

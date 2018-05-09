50f4619f92cdbe110af8efd539851b605137f9b8
Paul S <contact+github@winterei.se>
2018-05-10 08:23:46 +0900

    Add seeding for blank@spectero.com.

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,50 +15,50 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-      factory(App\User::class, 5)->create();
+        factory(App\User::class, 5)->create();
 
-      foreach(App\User::all() as $user)
-      {
+        foreach(App\User::all() as $user)
+        {
           PermissionManager::assign($user, UserRoles::USER);
-      }
+        }
 
-      $admin = \App\User::create([
+        $admin = \App\User::create([
           'name' => "Spectero Dev",
           'email' => "dev@spectero.com",
           'password' => \Illuminate\Support\Facades\Hash::make('temppass'),
           'status' => \App\Constants\UserStatus::ACTIVE,
           'node_key' => \App\Libraries\Utility::getRandomString(2)
-      ]);
+        ]);
 
-      $this->addMeta($admin);
-      PermissionManager::assign($admin, UserRoles::ADMIN);
+        $this->addMeta($admin);
+        PermissionManager::assign($admin, UserRoles::ADMIN);
 
-      $anatolie = \App\User::create([
-          'name' => "Anatolie Diordita",
-          'email' => 'anatolie@spectero.com',
-          'password' => \Illuminate\Support\Facades\Hash::make('temppass'),
-          'status' => \App\Constants\UserStatus::ACTIVE,
-          'node_key' => \App\Libraries\Utility::getRandomString(2)
-      ]);
+        $anatolie = \App\User::create([
+                                          'name' => "Anatolie Diordita",
+                                          'email' => 'anatolie@spectero.com',
+                                          'password' => \Illuminate\Support\Facades\Hash::make('temppass'),
+                                          'status' => \App\Constants\UserStatus::ACTIVE,
+                                          'node_key' => \App\Libraries\Utility::getRandomString(2)
+                                      ]);
 
-      $this->addMeta($anatolie);
-      PermissionManager::assign($anatolie, UserRoles::USER);
+        $this->addMeta($anatolie);
+        PermissionManager::assign($anatolie, UserRoles::USER);
 
         $sergio = \App\User::create([
-            'name' => "Sergio Castro",
-            'email' => 'sergio@spectero.com',
-            'password' => \Illuminate\Support\Facades\Hash::make('temppass'),
-            'status' => \App\Constants\UserStatus::ACTIVE,
-            'node_key' => \App\Libraries\Utility::getRandomString(2)
-        ]);
+                                        'name' => "Sergio Castro",
+                                        'email' => 'sergio@spectero.com',
+                                        'password' => \Illuminate\Support\Facades\Hash::make('temppass'),
+                                        'status' => \App\Constants\UserStatus::ACTIVE,
+                                        'node_key' => \App\Libraries\Utility::getRandomString(2)
+                                    ]);
 
         $this->addMeta($sergio);
         PermissionManager::assign($sergio, UserRoles::USER);
 
-      foreach (\App\Constants\UserStatus::getConstants() as $user)
-      {
-          switch ($user)
-          {
+        foreach (\App\Constants\UserStatus::getConstants() as $user)
+        {
+            switch ($user)
+            {
               case \App\Constants\UserStatus::ACTIVE:
                   for ($i = 0; $i < 2; $i++)
                   {
@@ -83,42 +83,54 @@ class UsersTableSeeder extends Seeder
                                                    'node_key' => \App\Libraries\Utility::getRandomString(2)
                                                ]);
                   PermissionManager::assign($newUser, UserRoles::USER);
-          }
+            }
 
-          PermissionManager::assign($newUser, UserRoles::USER);
-      }
+            PermissionManager::assign($newUser, UserRoles::USER);
+        }
+
+        $blank = \App\User::create([
+                                        'name' => "Blank User",
+                                        'email' => 'blank@spectero.com',
+                                        'password' => \Illuminate\Support\Facades\Hash::make('temppass'),
+                                        'status' => \App\Constants\UserStatus::ACTIVE,
+                                        'node_key' => \App\Libraries\Utility::getRandomString(2)
+                                    ]);
+
+        $this->addMeta($blank);
+        PermissionManager::assign($blank, UserRoles::USER);
+
     }
 
-    private function addMeta(\App\User $user)
-    {
-        $stripeTokens = [
-            'tok_visa',
-            'tok_visa_debit',
-            'tok_mastercard',
-            'tok_mastercard_debit',
-            'tok_mastercard_prepaid',
-            'tok_amex',
-            'tok_discover',
-            'tok_diners',
-            'tok_jcb'
-        ];
-
-        try
+        private function addMeta(\App\User $user)
         {
-            UserMeta::addOrUpdateMeta($user,UserMetaKeys::AddressLineOne, env('LEGAL_COMPANY_ADDRESS_PARTIAL_1'));
-            UserMeta::addOrUpdateMeta($user,UserMetaKeys::AddressLineTwo, env('LEGAL_COMPANY_ADDRESS_PARTIAL_2'));
-            UserMeta::addOrUpdateMeta($user,UserMetaKeys::Organization, 'Spectero');
-            UserMeta::addOrUpdateMeta($user, UserMetaKeys::TaxIdentification, 'FRC-3-612A1521');
-            UserMeta::addOrUpdateMeta($user, UserMetaKeys::PreferredCurrency, 'USD');
-            UserMeta::addOrUpdateMeta($user, UserMetaKeys::City, 'Shinagawa-ku');
-            UserMeta::addOrUpdateMeta($user, UserMetaKeys::State, 'Tokyo');
-            UserMeta::addOrUpdateMeta($user, UserMetaKeys::Country, 'JP');
-            UserMeta::addOrUpdateMeta($user, UserMetaKeys::PostCode, 68005);
-            UserMeta::addOrUpdateMeta($user, UserMetaKeys::StripeCardToken, array_random($stripeTokens));
-        }
-        catch (\App\Errors\FatalException $e)
-        {
+            $stripeTokens = [
+                'tok_visa',
+                'tok_visa_debit',
+                'tok_mastercard',
+                'tok_mastercard_debit',
+                'tok_mastercard_prepaid',
+                'tok_amex',
+                'tok_discover',
+                'tok_diners',
+                'tok_jcb'
+            ];
 
-        }
+            try
+            {
+                UserMeta::addOrUpdateMeta($user,UserMetaKeys::AddressLineOne, env('LEGAL_COMPANY_ADDRESS_PARTIAL_1'));
+                UserMeta::addOrUpdateMeta($user,UserMetaKeys::AddressLineTwo, env('LEGAL_COMPANY_ADDRESS_PARTIAL_2'));
+                UserMeta::addOrUpdateMeta($user,UserMetaKeys::Organization, 'Spectero');
+                UserMeta::addOrUpdateMeta($user, UserMetaKeys::TaxIdentification, 'FRC-3-612A1521');
+                UserMeta::addOrUpdateMeta($user, UserMetaKeys::PreferredCurrency, 'USD');
+                UserMeta::addOrUpdateMeta($user, UserMetaKeys::City, 'Shinagawa-ku');
+                UserMeta::addOrUpdateMeta($user, UserMetaKeys::State, 'Tokyo');
+                UserMeta::addOrUpdateMeta($user, UserMetaKeys::Country, 'JP');
+                UserMeta::addOrUpdateMeta($user, UserMetaKeys::PostCode, 68005);
+                UserMeta::addOrUpdateMeta($user, UserMetaKeys::StripeCardToken, array_random($stripeTokens));
+            }
+            catch (\App\Errors\FatalException $e)
+            {
+
+            }
     }
 }

4d9809e461c3ecda22f7e239233bd8deb62242cd
Paul S <contact+github@winterei.se>
2018-06-02 19:08:20 +0900

    MAR-94: fix invalid pre-header text in sent emails.

--- resources/views/emails/layouts/master.blade.php
+++ resources/views/emails/layouts/master.blade.php
@@ -9,7 +9,7 @@
             <div class="content">
 
                 <!-- START CENTERED WHITE CONTAINER -->
-                <span class="preheader">This is preheader text. Some clients will show this text as a preview.</span>
+                <!-- <span class="preheader">This is preheader text. Some clients will show this text as a preview.</span> -->
                 <table class="main">
 
                     <!-- START MAIN CONTENT AREA -->

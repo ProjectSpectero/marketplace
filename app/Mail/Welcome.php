2cdf7f2640e9fa90dc03a4f99aba670f652d585c
Paul S <reisende+gitkraken@outlook.com>
2018-07-09 20:36:13 +0900

    MAR-143: add login URL to the standard welcome email.

--- app/Mail/Welcome.php
+++ app/Mail/Welcome.php
@@ -2,11 +2,15 @@
 
 namespace App\Mail;
 
+use App\Libraries\Utility;
+
 class Welcome extends BaseMail
 {
     public function build()
     {
         return $this->subject($this->formatTitle("Welcome!"))
-            ->view('emails.Welcome');
+            ->view('emails.Welcome', [
+                'loginUrl' => Utility::generateUrl('login', 'frontend')
+        ]);
     }
 }
--- resources/views/emails/Welcome.blade.php
+++ resources/views/emails/Welcome.blade.php
@@ -5,4 +5,5 @@
     <p>Welcome to Spectero! We're so glad to have you on our platform.</p>
 
     <p>You may now login and access the platform.</p>
+    <a target="_blank" href="{{ $loginUrl }}">Login now!</a>
 @endsection

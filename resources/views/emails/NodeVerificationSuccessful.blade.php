eace3f2d6159681510f96fd461c9e28e272899eb
Paul S <reisende+gitkraken@outlook.com>
2018-07-09 19:56:52 +0900

    MAR-145: fix node success message.

--- resources/views/emails/NodeVerificationSuccessful.blade.php
+++ resources/views/emails/NodeVerificationSuccessful.blade.php
@@ -4,7 +4,7 @@
     <h2>Node Verification Successful</h2>
 
     <p>Hi {{ $node->user->name }},</p>
-    <p>Congratulations! This email is to let you know that we have successfully verified your Spectero node <b>#{{ $node->id }} "{{ $node->friendly_name }}"</b> with IP address <b>{{ $node->ip }}</b></p>
+    <p>Congratulations! This email is to let you know that we have successfully verified your Spectero node <b>#{{ $node->id }}</b> with IP address <b>{{ $node->ip }}</b></p>
 
     <p>You may now update it and decide whether you wish to list it on our marketplace alongside other relevant parameters.</p>
     <a target="_blank" href="{{ $nodeUrl }}">View Node</a>

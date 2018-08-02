bb346bc99f892df96f14b4a73df7d57fdd1fc3ea
Paul S <contact+github@winterei.se>
2018-08-03 08:20:14 +0900

    MAR-208: fix alpha_dash_spaces validation (no trailing spaces either).

--- app/Validators/AlphaDashPlusSpacesValidator.php
+++ app/Validators/AlphaDashPlusSpacesValidator.php
@@ -8,6 +8,6 @@ class AlphaDashPlusSpacesValidator
 {
     public function validate ($attribute, $value, $parameters) : bool
     {
-        return preg_match('/^[\pL\-\_\s]+$/iu', $value);
+        return preg_match('/^(?! )[\pL0-9\-\_\s]+(?<! )$/iu', $value);
     }
 }
